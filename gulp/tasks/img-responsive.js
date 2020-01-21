ae8af716dcede6780af2ec80d76ec43abf7f4b72
Pavel <wol1414@gmail.com>
2020-01-21 19:03:40 +0300

    edit color

--- gulp/tasks/img-responsive.js
+++ gulp/tasks/img-responsive.js
@@ -14,7 +14,7 @@ module.exports = function () {
 			.pipe($.gp.responsive({
 				'*': [{
 					// Produce @2x images
-					width: '100%', quality: 75, progressive: true, rename: { prefix: '@2x/', },
+					width: '100%', quality: 99, progressive: true, rename: { prefix: '@2x/', },
 				},
 					//  {
 					// 	// Produce @1x images
